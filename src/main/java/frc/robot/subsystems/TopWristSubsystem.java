f416ba4b84f781cd12425aecc6abb1348ed0a6d8
diya-dhirar <156548294+diya-dhirar@users.noreply.github.com>
2024-01-23 19:11:40 -0500

    finished top wrist motor subsystem

--- src/main/java/frc/robot/subsystems/TopWristSubsystem.java
+++ src/main/java/frc/robot/subsystems/TopWristSubsystem.java
@@ -4,20 +4,26 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.topWristConstants;
+import com.ctre.phoenix6.signals.NeutralModeValue; 
 
 public class TopWristSubsystem extends SubsystemBase {
   /** Creates a new topWrist. */
   
+  public TalonFXConfiguration topWristConfig = new TalonFXConfiguration(); 
   private TalonFX topWrist = new TalonFX(topWristConstants.topWristID);  
   DutyCycleEncoder encoder = new DutyCycleEncoder(topWristConstants.topWristAbsoluteEncoder); 
   
   public TopWristSubsystem() {
-    
+    topWrist.getConfigurator().apply(topWristConfig); 
+
+    topWrist.setNeutralMode(NeutralModeValue.Brake);
+
   }
 
   @Override
