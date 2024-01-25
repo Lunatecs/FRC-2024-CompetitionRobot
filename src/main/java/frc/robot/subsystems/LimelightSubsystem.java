6fe2c3204670bfffc1d7869b269dd9b4c2957586
HunterH <103085473+Hunter21H@users.noreply.github.com>
2024-01-24 19:20:10 -0500

    Limelight stub code

---
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimelightSubsystem extends SubsystemBase {
+  
+  private NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+  private NetworkTableEntry NetWorkTx = limelight.getEntry("tx");
+  private NetworkTableEntry NetworkTy = limelight.getEntry("ty");
+  private NetworkTableEntry NetworkTa = limelight.getEntry("ta");
+  
+  
+
+  /** Creates a new LimelightSubsystem. */
+  public LimelightSubsystem() {
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+// pipeline funnies
+  public void switchPipe(int pipe){
+      limelight.getEntry("pipeline").setValue(pipe);
+  }
+
+  public double getPipe(){
+    return limelight.getEntry("getpipe").getDouble(0);
+    //slap into shuffle board wink wonk?
+  }
+
+
+// object tracking vars
+
+  public double GetTx(){
+        return NetWorkTx.getDouble(0.0);
+
+  }
+
+  public double GetTy(){
+        return NetworkTy.getDouble(0.0);
+
+  }
+  
+public double GetTa(){
+        return NetworkTa.getDouble(0.0);
+  }
+
+
+// April Tag Vars
+public double GetX(){
+  return limelight.getEntry("botpose").getDoubleArray(new double[6])[0];
+}
+
+public double GetY(){
+  return limelight.getEntry("botpose").getDoubleArray(new double[6])[1];
+}
+
+public double GetZ(){
+  return limelight.getEntry("botpose").getDoubleArray(new double[6])[3];
+}
+
+public double GetPitch(){
+ return limelight.getEntry("botpose").getDoubleArray(new double[6])[4];
+}
+
+public double GetRoll(){
+ return limelight.getEntry("botpose").getDoubleArray(new double[6])[5];
+}
+
+public double GetYaw(){
+  return limelight.getEntry("botpose").getDoubleArray(new double[6])[6];
+}
+}
